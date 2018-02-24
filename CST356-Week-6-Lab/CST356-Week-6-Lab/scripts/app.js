014fd169ed6aeae10b8367dd87913433d9cbe5f4
dylanbrandstetter <35245920+dylanbrandstetter@users.noreply.github.com>
2018-02-23 18:53:55 -0800

    Exercise 3 and EC

---
+++ CST356-Week-6-Lab/CST356-Week-6-Lab/scripts/app.js
@@ -0,0 +1,52 @@
+﻿
+function displayInfo() {
+
+    alert("displaying");
+
+    var firstName = document.getElementById("FirstName").value;
+    var lastName = document.getElementById("LastName").value;
+    var age = document.getElementById("Age").value;
+
+    var valid = true;
+    if (firstName == "") {
+        alert("Must enter a value for first name");
+        valid = false;
+    }
+
+    if (lastName == "") {
+        alert("Must enter a value for last name");
+        valid = false;
+    }
+
+    if (age == "") {
+        alert("Must enter a value for age");
+        valid = false;
+    }
+
+    var years = document.getElementsByName("YearInSchool");
+
+    var oneChecked = false;
+    for (var i = 0; i < years.length; ++i) {
+        if (years[i].checked) {
+            oneChecked = true;
+        }
+    }
+
+    if (!oneChecked)
+    {
+        alert("Must select a year");
+        valid = false;
+    }
+
+    if (!valid)
+    {
+        return false;
+    }
+
+    var summary = firstName + " " + lastName + " is " + age + " years old.";
+
+    document.getElementById("SummaryHead").innerText = "Summary";
+    document.getElementById("SummaryText").innerText = summary;
+
+    return false;
+}
